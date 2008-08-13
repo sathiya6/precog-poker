bc8c6f9beca9812e21008974758aabc1e6638237
kevin91liu <kevin91liu@bcbed88d-6953-0410-b48a-472db7bb305d>
2008-08-13 20:47:48 +0000

    read in tables from file

---
+++ Precog/src/precog/PrecogMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package precog;
+
+/**
+ *
+ * @author kevinl
+ */
+public class PrecogMain 
+{
+    public static void main(String... args)
+    {
+        Precog a = new Precog("a");
+        
+    }
+}
