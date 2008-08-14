177fc7dacd8df4a8215bc3cb46fdcbfcf77565aa
kevin91liu <kevin91liu@bcbed88d-6953-0410-b48a-472db7bb305d>
2008-08-14 18:29:58 +0000

    precog rate method should be fine now...

--- Precog/src/precog/PrecogMain.java
+++ Precog/src/precog/PrecogMain.java
@@ -20,5 +20,16 @@ public class PrecogMain
         Player[] c = {a, b};
         Game g = new Game(c);
         g.begin();
+        
+        //assert false : "assertions on";
+        
+        /*Card a = new Card(Card.Suit.CLUBS, Card.TWO);
+        Card b = new Card(Card.Suit.SPADES, Card.THREE);
+        Card c = new Card(Card.Suit.HEARTS, Card.FOUR);
+        Card d = new Card(Card.Suit.CLUBS, Card.FIVE);
+        Card e = new Card(Card.Suit.CLUBS, Card.SIX);
+        Card[] cards = {a,b,c,d,e};
+        Hand z = new Hand(cards);
+        System.out.println(Precog.suitlessHand(z.getBitCards()));*/
     }
 }
