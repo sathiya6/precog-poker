380e71beb08bf7d01814aa66d0f963eb4d83447b
talkingraisin <talkingraisin@bcbed88d-6953-0410-b48a-472db7bb305d>
2009-06-05 00:31:08 +0000

    Added 2-threaded version of enum_pos_opp_hands(), called enum_pos_opp_hands_2_threads(). This method uses the class Enum_Pos_Opp_Hands_7th_Bit, and speeds up the process of enumerating possible 5-card opponent hands by 20-25 milliseconds on a dual core 2.4 Ghz processor.

---
+++ trunk/PrecogFiveCardDraw/src/precog5/Enum_Pos_Opp_Hands_7th_Bit.java
@@ -0,0 +1,56 @@
+package precog5;
+
+public class Enum_Pos_Opp_Hands_7th_Bit implements Runnable 
+{
+	private long pool;
+	private long[] pos_opp_hands;
+	/**
+	 * Constructs a runnable calculator that finds possible 5-card opponent hands given a pool
+	 * of candidates.
+	 * 
+	 * @param pool a 41-bit long that contains possible opponent cards
+	 */
+	Enum_Pos_Opp_Hands_7th_Bit(long pool, long[] pos_opp_hands)
+	{
+		this.pool = pool;
+		// 41 choose 5 = 749398;
+		this.pos_opp_hands = pos_opp_hands;
+	}
+	
+	long[] get_pos_opp_hands()
+	{
+		return pos_opp_hands;
+	}
+	
+	@Override
+	public void run() 
+	{
+		// since 1533939 - 749398 = 784541,
+		// this index is the start of the second half, which is what we're working on
+		int index = 784541;		
+		long c1, c2, c3, c4, c5, p1, p2, p3, p4, p5;
+		c1 = pool;
+		
+		for (int i = 0; i < 37; i++)
+		{
+			c2 = c1 ^= p1 = c1 & -c1;
+			for (int j = i + 1; j < 38; j++)
+			{
+				c3 = c2 ^= p2 = c2 & -c2;
+				for (int k = j + 1; k < 39; k++)
+				{
+					c4 = c3 ^= p3 = c3 & -c3;
+					for (int l = k + 1; l < 40; l++)
+					{
+						c5 = c4 ^= p4 = c4 & -c4;
+						for (int m = l + 1; m < 41; m++)
+						{
+							c5 ^= p5 = c5 & -c5;
+							pos_opp_hands[index++] = p1 | p2 | p3 | p4 | p5;							
+						}
+					}
+				}
+			}
+		}
+	}
+}
